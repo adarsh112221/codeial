8d13cf8cea41a360db4a56d6f30596095b14e7f5
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-08-23 11:33:36 +0530

    created some more actions

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function (req, res) {
-  return res.end("<h1>express is up for codeial</h1>");
+  return res.render("home", {
+    title: "home",
+  });
 };
 
 //module.exports.actionName=function(req,res){}
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use('/',require('./routes'))
 
 //setup the view engine
 app.set('view engine','ejs');
-app.set('view','./views');
+app.set('views','./views');
 
 app.listen(port,function(err)
 {
