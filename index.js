8b1a6ff4dfeee4b924967d0b1368e088f0a6b09d
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-08-26 15:15:57 +0530

    add cookies parser

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express');
+const express = require("express");
+const cookieparser = require("cookie-parser");
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
 
-app.use(express.static('./assets'));
+app.use(express.urlencoded())
+app.use(cookieparser())
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 // set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
 
-app.listen(port, function(err){
-    if (err){
-        console.log(`Error in running the server: ${err}`);
-    }
-
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
