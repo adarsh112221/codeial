b88d7e925b480ca760bd69efd75707d0c6694a33
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-08-31 20:25:31 +0530

    showing posts comment

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,23 +1,29 @@
 const Post = require("../models/post");
 module.exports.home = function (req, res) {
-  Post.find({},function(err,posts)
-  {
-  return res.render('home',{
-    title:" Home",
-    posts:posts
-  })
+  //Post.find({},function(err,posts)
+  //{
+  // return res.render('home',{
+  //   title:" Home",
+  //   posts:posts
+  // })
 
-  })
+  //})
 
   //populate the user of each
-  // Post.find({})
-  //   .populate("user")
-  //   .exec(function (err, posts) {
-  //     return res.render("home", {
-  //       title: " Home",
-  //       posts: posts,
-  //     });
-  //   });
+  Post.find({})
+    .populate("user")
+    .populate({
+      path:'comments',
+      populate:{
+        path:'user'
+      }
+    })
+    .exec(function (err, posts) {
+      return res.render("home", {
+        title: " Home",
+        posts: posts,
+      });
+    });
 };
 
 //module.exports.actionName=function(req,res){}
