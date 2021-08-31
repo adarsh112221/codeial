57c0648da12b2e8cdfc02f17433a4a6a0a632c0d
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-08-31 18:57:16 +0530

    added comments to database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,23 +1,23 @@
 const Post = require("../models/post");
 module.exports.home = function (req, res) {
-  // Post.find({},function(err,posts)
-  // {
-  // return res.render('home',{
-  //   title:" Home",
-  //   posts:posts
-  // })
+  Post.find({},function(err,posts)
+  {
+  return res.render('home',{
+    title:" Home",
+    posts:posts
+  })
 
-  // })
+  })
 
   //populate the user of each
-  Post.find({})
-    .populate("user")
-    .exec(function (err, posts) {
-      return res.render("home", {
-        title: " Home",
-        posts: posts,
-      });
-    });
+  // Post.find({})
+  //   .populate("user")
+  //   .exec(function (err, posts) {
+  //     return res.render("home", {
+  //       title: " Home",
+  //       posts: posts,
+  //     });
+  //   });
 };
 
 //module.exports.actionName=function(req,res){}
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get("/", homeController.home);
 router.get("/notifications", homeController.notifications);
 router.use("/users", require("./users"));
 router.use("/posts", require("./posts"));
+router.use("/comments", require("./comments"));
 
 //for any further routes access from here
 //router.use('/routeName,require('./routerfile'))
