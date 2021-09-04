fa2a34e6ed3b3d4f0effd2f6228148af762e596d
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-09-04 11:51:16 +0530

    converting to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post = require("../models/post");
 const User = require("../models/user");
-module.exports.home = function (req, res) {
+module.exports.home = async function (req, res) {
   //Post.find({},function(err,posts)
   //{
   // return res.render('home',{
@@ -11,23 +11,26 @@ module.exports.home = function (req, res) {
   //})
 
   //populate the user of each
-  Post.find({})
-    .populate("user")
-    .populate({
-      path: "comments",
-      populate: {
-        path: "user",
-      },
-    })
-    .exec(function (err, posts) {
-      User.find({}, function (err, users) {
-        return res.render("home", {
-          title: " Home",
-          posts: posts,
-          all_users: users,
-        });
+  try {
+    let posts = await Post.find({})
+      .populate({path:"user"})
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
       });
+
+    let users = await User.find({});
+
+    return res.render("home", {
+      title: " Home",
+      posts: posts,
+      all_users: users,
     });
+  } catch (err) { 
+    console.log("Error", err);
+  }
 };
 
 //module.exports.actionName=function(req,res){}
