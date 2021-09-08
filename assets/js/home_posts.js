6cad875ac062b0418d59cf9649a8cb6baf29b9ca
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-09-08 13:13:14 +0530

    showing the data ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -8,7 +8,8 @@
           url:'/posts/create',
           data:newPostForm.serialize(),
           success:function(data){
-              console.log(data);
+            let newPost=newPostDom(data.data.post);
+            $('#posts-list-container>ul').prepend(newPost);
           },
           error: function(error)
           {
@@ -19,5 +20,34 @@
     });
   };
   //method to create a post in dom
+  let newPostDom=function(post)
+  {
+    return $(`<li id="post-${post._id}">
+    <p>
+     
+      <small>
+        <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
+      </small>
+      ${post.content}
+  </br>
+  <small>
+  ${post.user.name}
+  </small>
+  </p>
+  <div class="post-comments">
+   <form action="/comments/create" method="POST">
+   <input type="text" name="content" placeholder="Type Here To Add Comment ...">
+   <input type="hidden" value="${post._id}" name="post">
+   <input type="submit" value="Add Comment">
+     
+  </form>
+  <div class="post-comments-list">
+    <ul id="post-comment-${post._id}">
+    </ul>
+  </div>
+  </div></li>`)}
+    
+
+
   createPost();
 }
