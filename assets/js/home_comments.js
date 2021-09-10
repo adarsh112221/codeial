1bb256b86f4c52be19f4d42d2fc74bea8ea2811f
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-09-10 11:24:50 +0530

    added create comment through ajax

--- assets/js/home_comments.js
+++ assets/js/home_comments.js
@@ -1,16 +1,18 @@
 let createComment = function (postId) {
   let newCommentForm = $(`#post-${postId}-comments-form`);
+
   newCommentForm.submit(function (e) {
     e.preventDefault();
+    console.log(newCommentForm.serialize());
     $.ajax({
       type: "post",
       url: "/comments/create",
       data: newCommentForm.serialize(),
       success: function (data) {
         let commentx = data.data.comment;
-        console.log(data.data);
+        console.log(data.data.comment.content);
         let newComment = newCommentDom(commentx);
-        $(`#post-comments-list>post-comment-${postId}`).prepend(newComment);
+        $(`#post-comments-${postId}`).prepend(newComment);
       },
       error: function (error) {
         console.log(error.responseText);
@@ -19,13 +21,20 @@ let createComment = function (postId) {
   });
 };
 let newCommentDom = function (comment) {
-  return $(`<li  id="comment-${comment._id}>
-   <p>
-     <a class="delete-comment-button" href="/comments/destroy/${comment._id}">x</a>
-     ${comment.content}
-     <br />
-     <small> ${comment.user} </small>
-   </p>
- </li>`);
+  return $(`<li id="comment-${ comment._id }">
+  <p>
+      
+      <small>
+          <a class="delete-comment-button" href="/comments/destroy/${comment._id}">X</a>
+      </small>
+      
+      ${comment.content}
+      <br>
+      <small>
+          ${comment.user.name}
+      </small>
+  </p>    
+
+</li>`);
 };
 createComment();
