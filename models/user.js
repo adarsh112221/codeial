0e7347a31e9da31b903701fe80440890fa5e2888
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-09-12 10:13:51 +0530

    adding multer

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-
+const multer=require('multer');
+const path=require('path');
+const AVATAR_PATH=path.join('/uploads/user/avatars')
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
