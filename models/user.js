055107b55a34c95d82f1ed73a5eeb544ed455dee
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-09-12 10:35:34 +0530

    setting up multer in users schema

--- models/user.js
+++ models/user.js
@@ -1,24 +1,40 @@
 const mongoose = require("mongoose");
-const multer=require('multer');
-const path=require('path');
-const AVATAR_PATH=path.join('/uploads/user/avatars')
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    require: true,
-    unique: true,
-  },
-  password:{
-      type:String,
-      required:true,
-  },
-  name:{
-      type:String,
-      required:true
-  }},
+const multer = require("multer");
+const path = require("path");
+const AVATAR_PATH = path.join("/uploads/user/avatars");
+const userSchema = new mongoose.Schema(
   {
-      timestamps:true
-  
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
 });
-const User=mongoose.model("User",userSchema);
-module.exports=User;
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
