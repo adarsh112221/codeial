b242bcc711d7ad213bb6eed67d73239d978bcf44
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-09-14 12:31:23 +0530

    added google-oauth

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,54 @@
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const crypto = require("crypto");
+const User = require("../models/user");
+
+
+//tell passport to use a new strategy to google login
+passport.use(
+    //use these details provided by the google 
+  new googleStrategy(
+    {
+      clientID:
+        "813904193880-cb8se9t7a3nsjkk3g0canijqt2b1nsrt.apps.googleusercontent.com",
+      clientSecret: "vEzH-632MOKI1jJX8kk4rXwa",
+      callbackURL: "http://localhost:8000/users/auth/google/callback",
+    },
+    function (accessToken, refershToken, profile, done) {
+        
+      User.findOne({ email: profile.emails[0].value }).exec(function (
+        err,
+        user
+      ) {
+        if (err) {
+          console.log("error in google strategy-passport", err);
+          return;
+        }
+        console.log(profile);
+        if (user) {
+            //find a user if found set this user as req.user
+          return done(null, user);
+        } else {
+            //if not create this user 
+          User.create(
+            {
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              passport: crypto.randomBytes[20].toString("hex"),
+            },
+            function (err, user) {
+              if (err) {
+                console.log("error in finding the user");
+                return;
+              } else {
+                return done(null, user);
+              }
+            }
+          );
+        }
+      });
+    }
+  )
+);
+
+module.exports = passport;
