08d4eae526f82780f477a3e6e6f18ef03f4145c5
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-10-09 11:52:40 +0530

    added enviorment variable

--- config/enviornment.js
+++ config/enviornment.js
@@ -26,5 +26,23 @@ const development = {
 };
 const production = {
   name: "enviornment",
+  asset_path: process.env.CODEIAL_ASSETS_PATH,
+  session_cookie_key: process.env.CODEIAL_SESSION_COOKI,
+  db: "codeial_production",
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.CODEIAL_AUTH_USER,
+      pass: process.env.CODEIAL_AUTH_PASSWORD,
+    },
+  },
+  google_client_id:
+    "813904193880-cb8se9t7a3nsjkk3g0canijqt2b1nsrt.apps.googleusercontent.com",
+  google_client_secret: "vEzH-632MOKI1jJX8kk4rXwa",
+  google_client_call_back_url: "http://codeial.com/users/auth/google/callback",
+  jwt_secret: process.env.CODEIAL_JWT_SECRET,
 };
 module.exports = development;
