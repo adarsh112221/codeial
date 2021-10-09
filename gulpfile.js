483359dd5334f4744ab5fa4f97c9c5c8394ed95d
adarsh112221 <adarsh.19b401007@abes.ac.in>
2021-10-09 22:04:00 +0530

    minfied and compressed images and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
+const uglify = require("gulp-uglify-es").default;
 const rev = require("gulp-rev");
-
-gulp.task("css", function () {
+const del = require("del");
+const imagemin = require("gulp-imagemin");
+gulp.task("css", function (done) {
   console.log("minifying css.....");
   gulp
     .src("./assets/sass/**/*.scss")
     .pipe(sass())
     .pipe(cssnano())
     .pipe(gulp.dest("./assets.css"));
-  return gulp
+  gulp
     .src("./assets/**/*.css")
     .pipe(rev())
     .pipe(gulp.dest("./public/assets"))
@@ -21,4 +23,52 @@ gulp.task("css", function () {
       })
     )
     .pipe(gulp.dest("./public/assets"));
+  done();
 });
+gulp.task("js", function (done) {
+  console.log("minifying js.....");
+  gulp
+    .src("./assets/**/*.js")
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest("./public/assets"))
+    .pipe(
+      rev.manifest({
+        cwd: "public",
+        merge: true,
+      })
+    )
+    .pipe(gulp.dest("./public/assets"));
+  done();
+});
+gulp.task("images", function (done) {
+  console.log("compressing images....."); //this is regex
+  gulp
+    .src("./assets/**/*.+(png|jpg|gif|svg|jpeg)")
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest("./public/assets"))
+    .pipe(
+      rev.manifest({
+        cwd: "public",
+        merge: true,
+      })
+    )
+    .pipe(gulp.dest("./public/assets"));
+  done();
+});
+
+//this is for emptying the public/assets directary
+gulp.task("clean:assets", function (done) {
+  del.sync("./public/assets");
+  done();
+});
+
+gulp.task(
+  "build",
+  gulp.series("clean:assets", "css", "js", "images"),
+  function (done) {
+    console.log("Building assets");
+    done();
+  }
+);
